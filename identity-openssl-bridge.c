6705d4f3cdb520188f76f92b961e1c606e4a3bfa
Richard Levenberg <richardl@ufp.com>
2017-09-01 11:04:05 -0700

    Moved send_message code into do/while in order to error out with breaks

--- identity-openssl-bridge.c
+++ identity-openssl-bridge.c
@@ -158,194 +158,190 @@ char *send_message(identity_context_t * identity_context, char *path, StrMap * p
 
     BIO *web = NULL;
     SSL *ssl = NULL;
+    do {
+        web = BIO_new_ssl_connect(identity_context->ssl_ctx);
+        ssl_err = ERR_get_error();
 
-    web = BIO_new_ssl_connect(identity_context->ssl_ctx);
-    ssl_err = ERR_get_error();
-
-    ASSERT(web != NULL);
-    if (!(web != NULL)) {
-        print_error_string(ssl_err, "BIO_new_ssl_connect");
-        //break; /* failed */
-    }
-
-    /* https://www.openssl.org/docs/crypto/BIO_s_connect.html */
-    char hostname[256];
-    sprintf(hostname, "%s:%d", get_host(), HOST_PORT);
-    res = BIO_set_conn_hostname(web, hostname);
-    ssl_err = ERR_get_error();
-
-    ASSERT(1 == res);
-    if (!(1 == res)) {
-        print_error_string(ssl_err, "BIO_set_conn_hostname");
-        //break; /* failed */
-    }
-
-    /* https://www.openssl.org/docs/crypto/BIO_f_ssl.html */
-    /* This copies an internal pointer. No need to free.  */
-    BIO_get_ssl(web, &ssl);
-    ssl_err = ERR_get_error();
-
-    ASSERT(ssl != NULL);
-    if (!(ssl != NULL)) {
-        print_error_string(ssl_err, "BIO_get_ssl");
-        //break; /* failed */
-    }
-
-    /* https://www.openssl.org/docs/ssl/ssl.html#DEALING_WITH_PROTOCOL_CONTEXTS */
-    /* https://www.openssl.org/docs/ssl/SSL_CTX_set_cipher_list.html            */
-    res = SSL_set_cipher_list(ssl, PREFERRED_CIPHERS);
-    ssl_err = ERR_get_error();
-
-    ASSERT(1 == res);
-    if (!(1 == res)) {
-        print_error_string(ssl_err, "SSL_set_cipher_list");
-        //break; /* failed */
-    }
-
-    /* No documentation. See the source code for tls.h and s_client.c */
-    res = SSL_set_tlsext_host_name(ssl, get_host());
-    ssl_err = ERR_get_error();
-
-    ASSERT(1 == res);
-    if (!(1 == res)) {
-        /* Non-fatal, but who knows what cert might be served by an SNI server  */
-        /* (We know its the default site's cert in Apache and IIS...)           */
-        print_error_string(ssl_err, "SSL_set_tlsext_host_name");
-        /* break; */
-    }
-
-    /* https://www.openssl.org/docs/crypto/BIO_s_file.html */
-    out = BIO_new_fp(stdout, BIO_NOCLOSE);
-    ssl_err = ERR_get_error();
-
-    ASSERT(NULL != out);
-    if (!(NULL != out)) {
-        print_error_string(ssl_err, "BIO_new_fp");
-        //break; /* failed */
-    }
-
-    /* https://www.openssl.org/docs/crypto/BIO_s_connect.html */
-    res = BIO_do_connect(web);
-    ssl_err = ERR_get_error();
-
-    ASSERT(1 == res);
-    if (!(1 == res)) {
-        print_error_string(ssl_err, "BIO_do_connect");
-        //break; /* failed */
-    }
-
-    /* https://www.openssl.org/docs/crypto/BIO_f_ssl.html */
-    res = BIO_do_handshake(web);
-    ssl_err = ERR_get_error();
-
-    ASSERT(1 == res);
-    if (!(1 == res)) {
-        print_error_string(ssl_err, "BIO_do_handshake");
-        //break; /* failed */
-    }
-
-    /**************************************************************************************/
-    /**************************************************************************************/
-    /* You need to perform X509 verification here. There are two documents that provide   */
-    /*   guidance on the gyrations. First is RFC 5280, and second is RFC 6125. Two other  */
-    /*   documents of interest are:                                                       */
-    /*     Baseline Certificate Requirements:                                             */
-    /*       https://www.cabforum.org/Baseline_Requirements_V1_1_6.pdf                    */
-    /*     Extended Validation Certificate Requirements:                                  */
-    /*       https://www.cabforum.org/Guidelines_v1_4_3.pdf                               */
-    /*                                                                                    */
-    /* Here are the minimum steps you should perform:                                     */
-    /*   1. Call SSL_get_peer_certificate and ensure the certificate is non-NULL. It      */
-    /*      should never be NULL because Anonymous Diffie-Hellman (ADH) is not allowed.   */
-    /*   2. Call SSL_get_verify_result and ensure it returns X509_V_OK. This return value */
-    /*      depends upon your verify_callback if you provided one. If not, the library    */
-    /*      default validation is fine (and you should not need to change it).            */
-    /*   3. Verify either the CN or the SAN matches the host you attempted to connect to. */
-    /*      Note Well (N.B.): OpenSSL prior to version 1.1.0 did *NOT* perform hostname   */
-    /*      verification. If you are using OpenSSL 0.9.8 or 1.0.1, then you will need     */
-    /*      to perform hostname verification yourself. The code to get you started on     */
-    /*      hostname verification is provided in print_cn_name and print_san_name. Be     */
-    /*      sure you are sensitive to ccTLDs (don't navively transform the hostname       */
-    /*      string). http://publicsuffix.org/ might be helpful.                           */
-    /*                                                                                    */
-    /* If all three checks succeed, then you have a chance at a secure connection. But    */
-    /*   its only a chance, and you should either pin your certificates (to remove DNS,   */
-    /*   CA, and Web Hosters from the equation) or implement a Trust-On-First-Use (TOFU)  */
-    /*   scheme like Perspectives or SSH. But before you TOFU, you still have to make     */
-    /*   the customary checks to ensure the certifcate passes the sniff test.             */
-    /*                                                                                    */
-    /* Happy certificate validation hunting!                                              */
-    /**************************************************************************************/
-    /**************************************************************************************/
-
-
-    /* Step 1: verify a server certifcate was presented during negotiation */
-    /* https://www.openssl.org/docs/ssl/SSL_get_peer_certificate.html          */
-    X509 *cert = SSL_get_peer_certificate(ssl);
-    if (cert) {
-        X509_free(cert);
-    }                           // free immediately
-
-    ASSERT(NULL != cert);
-    if (NULL == cert) {
-        /* Hack a code for print_error_string. */
-        print_error_string(X509_V_ERR_APPLICATION_VERIFICATION, "SSL_get_peer_certificate");
-        //break; /* failed */
-    }
-
-    /* Step 2: verify the result of chain verifcation             */
-    /* http://www.openssl.org/docs/ssl/SSL_get_verify_result.html */
-    /* Error codes: http://www.openssl.org/docs/apps/verify.html  */
-    res = SSL_get_verify_result(ssl);
-
-    //ASSERT(X509_V_OK == res);
-    if (!(X509_V_OK == res)) {
-        /* Hack a code into print_error_string. */
-        print_error_string((unsigned long) res, "SSL_get_verify_results");
-        //break; /* failed */
-    }
-
-    /* Step 3: hostname verifcation.   */
-    /* An exercise left to the reader. */
-    cert = SSL_get_peer_certificate(ssl);
-    if (cert) {
-        X509_NAME *subject = X509_get_subject_name(cert);
-        unsigned char *name = get_cn_name("Peer certificate", subject);
-        bool ufp = endsWith(name, "ufp.com");
-
-        OPENSSL_free(name);
-        X509_free(cert);
-
-        ASSERT(ufp);
-        if (!ufp) {
-            print_error_string(X509_V_ERR_APPLICATION_VERIFICATION, "peer certificate not valid");
-            //break;
+        ASSERT(web != NULL);
+        if (!(web != NULL)) {
+            print_error_string(ssl_err, "BIO_new_ssl_connect");
+            break; /* failed */
         }
+
+        /* https://www.openssl.org/docs/crypto/BIO_s_connect.html */
+        char hostname[256];
+        sprintf(hostname, "%s:%d", get_host(), HOST_PORT);
+        res = BIO_set_conn_hostname(web, hostname);
+        ssl_err = ERR_get_error();
+
+        ASSERT(1 == res);
+        if (!(1 == res)) {
+            print_error_string(ssl_err, "BIO_set_conn_hostname");
+            break; /* failed */
+        }
+
+        /* https://www.openssl.org/docs/crypto/BIO_f_ssl.html */
+        /* This copies an internal pointer. No need to free.  */
+        BIO_get_ssl(web, &ssl);
+        ssl_err = ERR_get_error();
+
+        ASSERT(ssl != NULL);
+        if (!(ssl != NULL)) {
+            print_error_string(ssl_err, "BIO_get_ssl");
+            break; /* failed */
+        }
+
+        /* https://www.openssl.org/docs/ssl/ssl.html#DEALING_WITH_PROTOCOL_CONTEXTS */
+        /* https://www.openssl.org/docs/ssl/SSL_CTX_set_cipher_list.html            */
+        res = SSL_set_cipher_list(ssl, PREFERRED_CIPHERS);
+        ssl_err = ERR_get_error();
+
+        ASSERT(1 == res);
+        if (!(1 == res)) {
+            print_error_string(ssl_err, "SSL_set_cipher_list");
+            break; /* failed */
+        }
+
+        /* No documentation. See the source code for tls.h and s_client.c */
+        res = SSL_set_tlsext_host_name(ssl, get_host());
+        ssl_err = ERR_get_error();
+
+        ASSERT(1 == res);
+        if (!(1 == res)) {
+            /* Non-fatal, but who knows what cert might be served by an SNI server  */
+            /* (We know its the default site's cert in Apache and IIS...)           */
+            print_error_string(ssl_err, "SSL_set_tlsext_host_name");
+            /* break; */
+        }
+
+        res = BIO_do_connect(web);
+        ASSERT(1 == res);
+        if (!(1 == res)) {
+            print_error_string(ssl_err, "BIO_do_connect");
+            break; /* failed */
+        }
+
+        /* https://www.openssl.org/docs/crypto/BIO_f_ssl.html */
+        res = BIO_do_handshake(web);
+        ssl_err = ERR_get_error();
+
+        ASSERT(1 == res);
+        if (!(1 == res)) {
+            print_error_string(ssl_err, "BIO_do_handshake");
+            break; /* failed */
+        }
+
+        /**************************************************************************************/
+        /**************************************************************************************/
+        /* You need to perform X509 verification here. There are two documents that provide   */
+        /*   guidance on the gyrations. First is RFC 5280, and second is RFC 6125. Two other  */
+        /*   documents of interest are:                                                       */
+        /*     Baseline Certificate Requirements:                                             */
+        /*       https://www.cabforum.org/Baseline_Requirements_V1_1_6.pdf                    */
+        /*     Extended Validation Certificate Requirements:                                  */
+        /*       https://www.cabforum.org/Guidelines_v1_4_3.pdf                               */
+        /*                                                                                    */
+        /* Here are the minimum steps you should perform:                                     */
+        /*   1. Call SSL_get_peer_certificate and ensure the certificate is non-NULL. It      */
+        /*      should never be NULL because Anonymous Diffie-Hellman (ADH) is not allowed.   */
+        /*   2. Call SSL_get_verify_result and ensure it returns X509_V_OK. This return value */
+        /*      depends upon your verify_callback if you provided one. If not, the library    */
+        /*      default validation is fine (and you should not need to change it).            */
+        /*   3. Verify either the CN or the SAN matches the host you attempted to connect to. */
+        /*      Note Well (N.B.): OpenSSL prior to version 1.1.0 did *NOT* perform hostname   */
+        /*      verification. If you are using OpenSSL 0.9.8 or 1.0.1, then you will need     */
+        /*      to perform hostname verification yourself. The code to get you started on     */
+        /*      hostname verification is provided in print_cn_name and print_san_name. Be     */
+        /*      sure you are sensitive to ccTLDs (don't navively transform the hostname       */
+        /*      string). http://publicsuffix.org/ might be helpful.                           */
+        /*                                                                                    */
+        /* If all three checks succeed, then you have a chance at a secure connection. But    */
+        /*   its only a chance, and you should either pin your certificates (to remove DNS,   */
+        /*   CA, and Web Hosters from the equation) or implement a Trust-On-First-Use (TOFU)  */
+        /*   scheme like Perspectives or SSH. But before you TOFU, you still have to make     */
+        /*   the customary checks to ensure the certifcate passes the sniff test.             */
+        /*                                                                                    */
+        /* Happy certificate validation hunting!                                              */
+        /**************************************************************************************/
+        /**************************************************************************************/
+
+
+        /* Step 1: verify a server certifcate was presented during negotiation */
+        /* https://www.openssl.org/docs/ssl/SSL_get_peer_certificate.html          */
+        X509 *cert = SSL_get_peer_certificate(ssl);
+        if (cert) {
+            X509_free(cert);
+        }                           // free immediately
+
+        ASSERT(NULL != cert);
+        if (NULL == cert) {
+            /* Hack a code for print_error_string. */
+            print_error_string(X509_V_ERR_APPLICATION_VERIFICATION, "SSL_get_peer_certificate");
+            break; /* failed */
+        }
+
+        /* Step 2: verify the result of chain verifcation             */
+        /* http://www.openssl.org/docs/ssl/SSL_get_verify_result.html */
+        /* Error codes: http://www.openssl.org/docs/apps/verify.html  */
+        res = SSL_get_verify_result(ssl);
+
+        //ASSERT(X509_V_OK == res);
+        if (!(X509_V_OK == res)) {
+            /* Hack a code into print_error_string. */
+            print_error_string((unsigned long) res, "SSL_get_verify_results");
+            break; /* failed */
+        }
+
+        /* Step 3: hostname verifcation.   */
+        /* An exercise left to the reader. */
+        cert = SSL_get_peer_certificate(ssl);
+        if (cert) {
+            X509_NAME *subject = X509_get_subject_name(cert);
+            unsigned char *name = get_cn_name("Peer certificate", subject);
+            bool ufp = endsWith(name, "ufp.com");
+
+            OPENSSL_free(name);
+            X509_free(cert);
+
+            ASSERT(ufp);
+            if (!ufp) {
+                print_error_string(X509_V_ERR_APPLICATION_VERIFICATION, "peer certificate not valid");
+                break;
+            }
+        }
+
+        /**************************************************************************************/
+        /**************************************************************************************/
+        /* Now, we can finally start reading and writing to the BIO...                        */
+        /**************************************************************************************/
+        /**************************************************************************************/
+        char *query = NULL;
+        char str[256];
+
+        sprintf(str, "POST %s HTTP/1.1\r\nHost: %s", path, get_host());
+        BIO_puts(web, str);
+        BIO_puts(web, "\r\nContent-Type: application/x-www-form-urlencoded");
+        query = create_query(parameters);
+        sprintf(str, "\r\nContent-Length: %zu", strlen(query));
+        BIO_puts(web, str);
+        BIO_puts(web, "\r\nConnection: close");
+        sprintf(str, "\r\n\r\n%s", query);
+        BIO_puts(web, str);
+
+        char *response = perform_read(web);
+        free(query);
+        sm_delete(parameters);
+        BIO_ssl_shutdown(web);
+        BIO_free_all(web);
+        return response;
+    } while (0);
+
+    if (web != NULL) {
+        BIO_ssl_shutdown(web);
+        BIO_free_all(web);
     }
+    return NULL;
 
-    /**************************************************************************************/
-    /**************************************************************************************/
-    /* Now, we can finally start reading and writing to the BIO...                        */
-    /**************************************************************************************/
-    /**************************************************************************************/
-    char *query = NULL;
-    char str[256];
-
-    sprintf(str, "POST %s HTTP/1.1\r\nHost: %s", path, get_host());
-    BIO_puts(web, str);
-    BIO_puts(web, "\r\nContent-Type: application/x-www-form-urlencoded");
-    query = create_query(parameters);
-    sprintf(str, "\r\nContent-Length: %zu", strlen(query));
-    BIO_puts(web, str);
-    sprintf(str, "\r\n\r\n%s", query);
-    BIO_puts(web, str);
-
-    char *response = perform_read(web);
-    free(query);
-    sm_delete(parameters);
-    BIO_free_all(web);
-    BIO_free(out);
-    return response;
 }
 
 #define READ_BUFFER_SIZE 4096
